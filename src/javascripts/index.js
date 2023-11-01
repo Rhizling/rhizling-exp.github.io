3adf2c128e0b3961414f248f84cf471172bc9622
Rhizling <saisakhamuri10@gmail.com>
2023-11-01 19:42:18 -0400

    Changed project section

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,1 +1,9 @@
-console.log('Hello from index.js!');
+document.querySelectorAll('.expandable-content').forEach((item) => {
+  console.log(item);
+  const summary = item.querySelector('.summary');
+  const details = item.querySelector('.details');
+
+  summary.addEventListener('click', () => {
+    details.style.display = details.style.display === 'block' ? 'none' : 'block';
+  });
+});
