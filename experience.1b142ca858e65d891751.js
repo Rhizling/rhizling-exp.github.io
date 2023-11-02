b19917f7f54ef563ff66e98380197bc5d5b5872b
Rhizling <saisakhamuri10@gmail.com>
2023-11-02 10:58:24 -0400

    Updates

---
+++ experience.1b142ca858e65d891751.js
@@ -0,0 +1,1 @@
+document.querySelectorAll(".expandable-content").forEach((function(e){console.log(e);var l=e.querySelector(".summary"),o=e.querySelector(".details");l.addEventListener("click",(function(){console.log("click"),o.style.display="block"===o.style.display?"none":"block"}))}));
--- index.cfc2661b7760217f9062.js
+++
@@ -1,1 +0,0 @@
-document.querySelectorAll(".expandable-content").forEach((function(e){console.log(e);var l=e.querySelector(".summary"),o=e.querySelector(".details");l.addEventListener("click",(function(){o.style.display="block"===o.style.display?"none":"block"}))}));
